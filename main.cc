4f42ca56520b33b4cf4ea0946adacdc8a04a1947
Jimmy Lu <gongchuo.lu@gmail.com>
2018-09-27 00:52:11 -0400

    Debug vector

--- main.cc
+++ main.cc
@@ -16,6 +16,17 @@ using tii = tuple<int, int>;
 #  define debug(expr) cerr << __FILE__ << ":" << __LINE__ << ": " << #expr << " = " << expr << endl
 #endif
 
+template <typename A>
+ostream & operator<<(ostream & out, vector<A> xs) {
+  out << "{";
+  for (size_t i = 0; i < xs.size(); i++) {
+    if (i > 0) out << ", ";
+    out << xs[i];
+  }
+  out << "}";
+  return out;
+}
+
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
