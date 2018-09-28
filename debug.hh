0ad76941c0288e58c5b0754b2490895012c63f1e
Jimmy Lu <gongchuo.lu@gmail.com>
2018-09-28 19:06:56 -0400

    Improve debug

---
+++ debug.hh
@@ -0,0 +1,55 @@
+#ifndef _DEBUG_HH_
+#define _DEBUG_HH_
+
+#define debug(...) \
+  cerr << __FILE__ << ":" << __LINE__ << ": {" << #__VA_ARGS__ << "} = ", _debug(__VA_ARGS__)
+
+string to_string(const char * s) {
+  string ans = "\"";
+  for (int i = 0; s[i]; i++) {
+    if (s[i] == '"') ans += "\\\"";
+    else ans += s[i];
+  }
+  ans += '"';
+  return ans;
+}
+
+string to_string(const string & s) {
+  return to_string(s.c_str());
+}
+
+template <typename A, typename B>
+string to_string(const pair<A, B> & kv) {
+  return "{" + to_string(kv.first) + "," + to_string(kv.second) + "}";
+}
+
+template <typename A>
+string to_string(const A & xs) {
+  string ans = "{";
+  bool first = true;
+  for (const auto & x: xs) {
+    if (!first) ans += ',';
+    first = false;
+    ans += to_string(x);
+  }
+  ans += '}';
+  return ans;
+}
+
+void _debug2() {
+  cerr << '}' << endl;
+}
+
+template <typename Head, typename... Tail>
+void _debug2(Head h, Tail... t) {
+  cerr << ", " << to_string(h);
+  _debug2(t...);
+}
+
+template <typename Head, typename... Tail>
+void _debug(Head h, Tail... t) {
+  cerr << '{' << to_string(h);
+  _debug2(t...);
+}
+
+#endif
--- main.cc
+++ main.cc
@@ -11,25 +11,12 @@ using namespace std;
 using tii = tuple<int, int>;
 
 #ifdef NDEBUG
-#  define debug(expr)
+#  define debug(...)
 #else
-#  define debug(expr) cerr << __FILE__ << ":" << __LINE__ << ": " << #expr << " = " << expr << endl
+#  include "debug.hh"
 #endif
 
-template <typename A>
-ostream & operator<<(ostream & out, vector<A> xs) {
-  out << "{";
-  for (size_t i = 0; i < xs.size(); i++) {
-    if (i > 0) out << ", ";
-    out << xs[i];
-  }
-  out << "}";
-  return out;
-}
-
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  cout << fixed;
+  ios_base::sync_with_stdio(false), cin.tie(0), cout << fixed;
   return 0;
 }
