319b3d34041979bb382635e32e1cadd6b34cead9
Jimmy Lu <gongchuo.lu@gmail.com>
2020-05-07 21:12:10 -0400

    Add debug for tuple

--- debug.hh
+++ debug.hh
@@ -1,36 +1,53 @@
 #pragma once
 
 #include <ostream>
+#include <tuple>
 
 #define DEBUG(x) ::std::cerr << __FILE__ ":" << __LINE__ << ": " << x << ::std::endl
 
-template <typename Iterator>
-struct Iterable {
-  Iterable(Iterator first, Iterator last): first_(first), last_(last) {
-  }
-private:
-  Iterator first_, last_;
-  template <typename Iterator2>
-  friend std::ostream& operator<<(std::ostream&, const Iterable<Iterator2>&);
-};
+namespace debug {
 
 template <typename Iterator>
-Iterable<Iterator> MakeIterable(Iterator first, Iterator last) {
-  return Iterable<Iterator>(first, last);
+struct Iterable {
+  Iterator first, last;
+};
+
+template <size_t...>
+struct IndexSequence {};
+
+template <size_t N, size_t... Is>
+struct MakeIndexSequence: MakeIndexSequence<N-1, N-1, Is...> {};
+
+template <size_t... Is>
+struct MakeIndexSequence<0, Is...>: IndexSequence<Is...> {};
+
+template <typename Tuple, size_t... Is>
+void PrintTuple(std::ostream& out, const Tuple& xs, IndexSequence<Is...>) {
+  using Expand = int[];
+  Expand{0, ((out << (Is == 0 ? "" : ",") << std::get<Is>(xs)), 0)...};
+}
+
 }
 
 template <typename Iterator>
-std::ostream& operator<<(std::ostream& out, const Iterable<Iterator>& xs) {
+debug::Iterable<Iterator> Iterable(Iterator first, Iterator last) {
+  return debug::Iterable<Iterator>{first, last};
+}
+
+template <typename Iterator>
+std::ostream& operator<<(std::ostream& out, const debug::Iterable<Iterator>& xs) {
   out << '{';
-  for (Iterator it = xs.first_; it != xs.last_; it++) {
-    if (it != xs.first_) out << ',';
+  for (Iterator it = xs.first; it != xs.last; it++) {
+    if (it != xs.first) out << ',';
     out << *it;
   }
   out << '}';
   return out;
 }
 
-template <typename T1, typename T2>
-std::ostream& operator<<(std::ostream& out, const std::pair<T1, T2>& xs) {
-  return out << '{' << xs.first << ',' << xs.second << '}';
+template <typename... Types>
+std::ostream& operator<<(std::ostream& out, const std::tuple<Types...>& xs) {
+  out << '{';
+  PrintTuple(out, xs, debug::MakeIndexSequence<sizeof...(Types)>{});
+  return out << '}';
 }
